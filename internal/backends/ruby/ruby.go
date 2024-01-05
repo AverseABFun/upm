c1c839dece6760db43bea00e652cc00042b821f0
Arthur Beck <averse.abfun@gmail.com>
2024-01-05 10:42:46 -0600

    Update ruby.go

--- internal/backends/ruby/ruby.go
+++ internal/backends/ruby/ruby.go
@@ -10,9 +10,9 @@ import (
 	"os/exec"
 	"strings"
 
-	"github.com/replit/upm/internal/api"
-	"github.com/replit/upm/internal/nix"
-	"github.com/replit/upm/internal/util"
+	"github.com/AverseABFun/upm/internal/api"
+	"github.com/AverseABFun/upm/internal/nix"
+	"github.com/AverseABFun/upm/internal/util"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
